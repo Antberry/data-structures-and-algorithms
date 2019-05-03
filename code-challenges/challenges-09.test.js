805e931b909b5710c9ca7144659229f2db0e7cbc
Antberry <aniej2013@gmail.com>
2019-05-03 14:58:33 -0700

    solved 3 thanks to michelle

--- code-challenges/challenges-09.test.js
+++ code-challenges/challenges-09.test.js
@@ -89,7 +89,7 @@ Note: You must use reduce for this challenge. You may not use the built-in .reve
 ------------------------------------------------------------------------------------------------ */
 
 const reversedString = (arr) => {
-  // Solution code here...
+  return arr.split('').reduce((ansSoFar,current)=> current + ansSoFar);
 };
 
 /* ------------------------------------------------------------------------------------------------
