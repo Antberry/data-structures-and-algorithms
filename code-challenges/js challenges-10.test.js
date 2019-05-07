fa8697e9803bd99d90c8d64dcdb5a82991ea8c74
Antberry <aniej2013@gmail.com>
2019-05-07 09:28:05 -0700

    solved 3

--- code-challenges/js challenges-10.test.js
+++ code-challenges/js challenges-10.test.js
@@ -12,7 +12,8 @@ For example, count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]]) returns 4.
 ------------------------------------------------------------------------------------------------ */
 
 const count = (target, input) => {
-  // Solution code here...
+  // filter this one
+
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -26,7 +27,7 @@ For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 ------------------------------------------------------------------------------------------------ */
 
 const totalSum = (input) => {
-  // Solution code here...
+  // filter for this one
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -42,8 +43,14 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
 const divisibleByFiveTwoToThePower = (input) => {
-  // Solution code here...
+  return input.map(array => {
+    return array.filter(element => element % 5 === 0);
+  })
+    .map(array => {
+      return array.map(element => Math.pow(2, element));
+    })
 };
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
@@ -108,7 +115,7 @@ let starWarsData = [{
 }];
 
 let findMaleAndFemale = (data) => {
-  // Solution code here...
+  return data.filter(element => element.gender === 'male' || element.gender === 'female').join(' and');
 };
 
 /* ------------------------------------------------------------------------------------------------
