dd5c11db721e3381256e6cf0ed708e087d896cd7
Antberry <aniej2013@gmail.com>
2019-07-01 10:36:03 -0700

    code challenge test complete

---
+++ 401codechallenges/src/main/java/Insertsort/InsertSort.java
@@ -0,0 +1,22 @@
+package Insertsort;
+
+public class InsertSort {
+
+    public int[] insertSort(int[] arr){
+        if (arr == null){
+            return null;
+        }else {
+            for (int i = 0; i < arr.length; i++) {
+                int j = i - 1;
+                int temp = arr[i];
+                while (j >= 0 && temp < arr[j]) {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+}
